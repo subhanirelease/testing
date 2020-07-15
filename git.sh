92b99193409bec6748e0d1f4ed6e4fefd8c8ca09
subhanirelease <subhani.s@releaseiq.io>
2020-07-15 15:57:06 +0530

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,38 @@
+echo "Enter a repo description: "
+read DESCRIPTION
+
+
+# step 2:  the local project folder path
+echo "what is the absolute path to your local project directory?"
+read PROJECT_PATH
+
+echo "What is your github username?"
+read USERNAME
+
+# step 3 : go to path
+cd "$PROJECT_PATH"
+
+
+# step 4: initialise the repo locally, create blank README, add and commit
+git init
+touch README.MD
+git add README.MD
+git commit -m 'initial commit -setup with .sh script'
+
+
+# step 5 use github API to log the user in
+git remote rm origin
+git remote rm origin1
+curl -u ${USERNAME} https://api.github.com/user/repos -d "{\"name\": \"${REPO_NAME}\", \"description\": \"${DESCRIPTION}\"}"
+git remote -v
+
+
+#  step 6 add the remote github repo to local repo and push
+git remote add origin https://github.com/${USERNAME}/${REPO_NAME}.git
+git push --set-upstream origin master
+
+# step 7 change to your project's root directory.
+cd "$PROJECT_PATH"
+
+echo "Done. Go to https://github.com/$USERNAME/$REPO_NAME to see."
+echo " *** You're no:w in your project root. ***"
