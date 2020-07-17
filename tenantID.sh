8ddae329911d62710e70defbfcaa054d9b4a77d0
subhanirelease <subhani.s@releaseiq.io>
2020-07-17 11:32:26 +0530

    Create tenantID.sh

---
+++ tenantID.sh
@@ -0,0 +1,1 @@
+curl --header 'Content-type: application/json' --request POST --data '{"enterpriseName" : "Subhani_Riq", "address1": "velachery", "address2": "medavakkam", "city": "chennai", "state" : "tamilnadu","country" : "India", "zipPostalCode": "600100", "createdBy": "subhani.s@releaseiq.io", "updatedBy": "sudhish@releaseiq.io"}' https://prod.api.releaseiq.io/cm/createEnterprise
